cb8c3567cf1639a514eee2659d5aa21bf17cf539
shinke-li <xinke.li@u.nus.edu>
2024-03-17 01:07:09 +0800

    update infer.sh

--- pointcvar/infer.sh
+++ pointcvar/infer.sh
@@ -1,18 +1,42 @@
 if [ ! -d "output" ]; then
 mkdir "output"
 fi
-
+#Test the model without any outlier removal
+gpu_id=3
 for model in 'pointnet' 'dgcnn'; do 
 
-for dataset in 'modelnet40' 'shapenet'; do
+    for dataset in 'modelnet40' 'shapenet'; do
 
-for cor in ${dataset}'_add_global' ${dataset}'_add_local' ${dataset}'_'${model}'_add_chamfer' ${dataset}'_'${model}'_add_cluster' ${dataset}'_'${model}'_add_hausdorff' ${dataset}'_'${model}'_add_object' ; do 
+        CUDA_VISIBLE_DEVICES=${gpu_id} python main.py --entry test --model-path ./runs/${dataset}_${model}.pth --exp-config configs/${dataset}_${model}.yaml --output ./output/${dataset}_${model}_clean.txt 
+    done 
 
-for sev in 5; do
+    for cor in 'add_global' 'add_local' ${model}'_add_chamfer' ${model}'_add_cluster' ${model}'_add_hausdorff' ${model}'_add_object' ; do 
 
-CUDA_VISIBLE_DEVICES=3 python main.py --entry test --model-path ./runs/${dataset}_${model}.pth --exp-config configs/corruption/${dataset}_${model}.yaml --severity ${sev} --corruption ${cor} --output ./output/${dataset}_${model}_clean_${cor}_${sev}.txt 
+        for sev in 5; do
 
+            CUDA_VISIBLE_DEVICES=${gpu_id} python main.py --entry test --model-path ./runs/${dataset}_${model}.pth --exp-config configs/corruption/${dataset}_${model}.yaml --severity ${sev} --corruption ${dataset}_${cor} --output ./output/${dataset}_${model}_${cor}_${sev}.txt 
+
+        done
+    done
 done
-done
-done
+
+#Test the model with outlier removal
+for model in 'pointnet' 'dgcnn'; do 
+
+    for robust in 'ror' 'srs' 'sor' 'cvar' 'iter_cvar'; do
+
+        for dataset in 'modelnet40' 'shapenet'; do
+
+            CUDA_VISIBLE_DEVICES=${gpu_id} python main.py --entry test --model-path ./runs/${dataset}_${model}.pth --exp-config configs/${dataset}_${model}.yaml --output ./output/${dataset}_${model}_${robust}_clean.txt --extra-config configs/infer/${robust}.yaml
+        done 
+
+        for cor in 'add_global' 'add_local' ${model}'_add_chamfer' ${model}'_add_cluster' ${model}'_add_hausdorff' ${model}'_add_object' ; do 
+
+            for sev in 5; do
+
+                CUDA_VISIBLE_DEVICES=${gpu_id} python main.py --entry test --model-path ./runs/${dataset}_${model}.pth --exp-config configs/corruption/${dataset}_${model}.yaml --severity ${sev} --corruption ${dataset}_${cor} --output ./output/${dataset}_${model}_${robust}_${cor}_${sev}.txt --extra-config configs/infer/${robust}.yaml
+
+            done
+        done 
+    done 
 done
--- pointcvar/infer_backdoor.sh
+++ pointcvar/infer_backdoor.sh
@@ -2,6 +2,8 @@ if [ ! -d "output" ]; then
 mkdir "output"
 fi
 
+gpu_id=3
+
 for model in 'backdoor_pointnet_pl' 'backdoor_pointnet_cl' 'backdoor_dgcnn_pl'; do 
 
 for dataset in 'modelnet40' 'shapenet'; do
@@ -10,8 +12,28 @@ for cor in ${dataset}'_backdoor'; do
 
 for sev in 5; do
 
-CUDA_VISIBLE_DEVICES=3 python main.py --entry test --model-path ./runs/${dataset}_${model}.pth --exp-config configs/corruption/${dataset}_${model}.yaml --severity ${sev} --corruption ${cor} --output ./output/${dataset}_${model}_clean_${cor}_${sev}.txt 
+CUDA_VISIBLE_DEVICES=${gpu_id} python main.py --entry test --model-path ./runs/${dataset}_${model}.pth --exp-config configs/corruption/${dataset}_${model}.yaml --severity ${sev} --corruption ${cor} --output ./output/${dataset}_${model}_clean_${cor}_${sev}.txt 
 
+done
+done
+done
+done
+
+
+
+for model in 'backdoor_pointnet_pl' 'backdoor_pointnet_cl' 'backdoor_dgcnn_pl'; do 
+
+for dataset in 'modelnet40' 'shapenet'; do
+
+for cor in ${dataset}'_backdoor'; do 
+
+for robust in 'ror' 'srs' 'sor' 'cvar' 'iter_cvar'; do
+
+for sev in 5; do
+
+CUDA_VISIBLE_DEVICES=${gpu_id} python main.py --entry test --model-path ./runs/${dataset}_${model}.pth --exp-config configs/corruption/${dataset}_${model}.yaml --severity ${sev} --corruption ${cor} --output ./output/${dataset}_${model}_${robust}_${cor}_${sev}.txt --extra-config configs/infer/${robust}.yaml
+
+done
 done
 done
 done
